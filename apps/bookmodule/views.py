63c4e7634a2fdf0ac434474bd94a63bf558c5151
ميسَم <412205633@qu.edu.sa>
2026-02-16 00:50:30 +0300

    Add index2 view to return value1 from request

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 def index(request):
     name = request.GET.get("name") or "world!"
     return HttpResponse("Hello, "+name)
+def index2(request, val1=0):
+    return HttpResponse("value1 = "+str(val1))
+
